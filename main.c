41d42275ac92f66ccf7422a5eb4696f0a4a4e4c1
Yuval <yuval.tasher@gmail.com>
2021-07-08 23:22:27 +0300

    Organize code and add a bad repl
    Organize code and add a simple (and bad) REPL
    
    Organize code and add a new (bad) REPL

--- main.c
+++ main.c
@@ -8,7 +8,8 @@ struct Tokenizer {
     /* read from a string or from a file? */
     enum {
         ReadFromString = 1,
-        ReadFromFile
+        ReadFromFile,
+        ReadFromInput
     } read_type;
     /* a union storing the file/string+idx */
     union {
@@ -58,6 +59,9 @@ struct Interpreter {
     size_t tape_idx;
 };
 
+/*
+  returns the current char of the stream
+*/
 static char tokenizer_get_char(struct Tokenizer *tokenizer) {
     char c;
 
@@ -66,15 +70,23 @@ static char tokenizer_get_char(struct Tokenizer *tokenizer) {
         if (c == '\0')
             return '\0';
         ++tokenizer->read_from.string;
-    } else {
+    } else if (tokenizer->read_type == ReadFromFile){
         if (feof(tokenizer->read_from.file))
             return '\0';
         c = fgetc(tokenizer->read_from.file);
+    } else {
+        /* TODO: add support for \r\n */
+        c = fgetc(stdin);
+        if (c == '\n')
+            return '\0';
     }
 
     return c;
 }
 
+/*
+  push a token into the tokenizer
+*/
 static void tokenizer_push_token(struct Tokenizer *tokenizer, enum TokenType token) {
     if (tokenizer->allocated == 0) {
         tokenizer->allocated = 64;
@@ -90,10 +102,13 @@ static void tokenizer_push_token(struct Tokenizer *tokenizer, enum TokenType tok
     tokenizer->tokens[tokenizer->idx++] = token;
     return;
 failure:
-    perror("tokenizer_push_token");
+    perror("malloc/realloc");
     exit(1);
 }
 
+/*
+  tokenize into tokenizer.tokens
+*/
 static void tokenizer_tokenize(struct Tokenizer *tokenizer) {
     char c;
 
@@ -128,34 +143,10 @@ static void tokenizer_tokenize(struct Tokenizer *tokenizer) {
     tokenizer_push_token(tokenizer, TokenTypeEof);
 }
 
-static enum TokenType *tokenize_string(char *string) {
-    struct Tokenizer tokenizer;
-
-    tokenizer.read_type = ReadFromString;
-    tokenizer.read_from.string = string;
-    tokenizer.allocated = tokenizer.idx = 0;
-    tokenizer_tokenize(&tokenizer);
-
-    return tokenizer.tokens;
-}
-
-static enum TokenType *tokenize_file(const char *filename) {
-    struct Tokenizer tokenizer;
-
-    tokenizer.read_type = ReadFromFile;
-    tokenizer.read_from.file = fopen(filename, "r"); /* open file */
-    if (!tokenizer.read_from.file) {
-        perror("tokenize_file");
-        exit(1);
-    }
-    tokenizer.allocated = tokenizer.idx = 0;
-    tokenizer_tokenize(&tokenizer);
-    fclose(tokenizer.read_from.file); /* close file */
-
-    return tokenizer.tokens;
-}
-
-static struct Action parser_read_action(struct Parser *parser) {
+/*
+  parse an action and return it
+*/
+static struct Action parser_parse_action(struct Parser *parser) {
     struct Action tmp;
 
     switch (parser->tokens[parser->token_read_idx++]) {
@@ -199,7 +190,7 @@ static struct Action parser_read_action(struct Parser *parser) {
                     allocated += 8;
                     tmp.loop_children = realloc(tmp.loop_children, allocated * sizeof(struct Action));
                 }
-                tmp.loop_children[w_idx++] = parser_read_action(parser);
+                tmp.loop_children[w_idx++] = parser_parse_action(parser);
                 break;
             }
         }
@@ -209,8 +200,7 @@ static struct Action parser_read_action(struct Parser *parser) {
             tmp.loop_children = realloc(tmp.loop_children, allocated * sizeof(struct Action));
             tmp.loop_children[w_idx].action = ActionEnd;
             ++w_idx;
-        }
-        break;
+        } break;
     }
     case TokenTypeCloseBracket:
         fprintf(stderr, "Unmatched ']'\n");
@@ -219,6 +209,9 @@ static struct Action parser_read_action(struct Parser *parser) {
     return tmp;
 }
 
+/*
+  push an action into parser.actions
+*/
 static void parser_push(struct Parser *parser, struct Action action) {
     if (parser->allocated == 0) {
         parser->allocated = 64;
@@ -230,16 +223,22 @@ static void parser_push(struct Parser *parser, struct Action action) {
     parser->actions[parser->idx++] = action;
 }
 
+/*
+  parse all actions and put them into parser.actions
+*/
 static void parser_parse(struct Parser *parser) {
     struct Action action;
     do {
-        action = parser_read_action(parser);
+        action = parser_parse_action(parser);
         parser_push(parser, action);
     } while (action.action != ActionEnd);
-    /* free tokens, because they aren't used anymore */
+    /* free tokens, because they have no use anymore */
     free(parser->tokens);
 }
 
+/*
+  high level function for parsing tokens
+*/
 static struct Action *parse_tokens(enum TokenType *tokens) {
     struct Parser parser;
 
@@ -252,6 +251,9 @@ static struct Action *parse_tokens(enum TokenType *tokens) {
     return parser.actions;
 }
 
+/*
+  execute an action
+*/
 static void interpreter_execute_action(struct Interpreter *interpreter, struct Action action) {
     switch (action.action) {
     case ActionIncrement:
@@ -294,49 +296,89 @@ static void interpreter_execute_action(struct Interpreter *interpreter, struct A
     case ActionPutChar:
         putchar(interpreter->tape[interpreter->tape_idx]);
         break;
+    default:
+        break;
     }
 }
 
+/*
+  execute all of the actions one by one
+*/
 static void interpreter_execute(struct Interpreter *interpreter) {
-    while (interpreter->actions[interpreter->action_read_idx].action != ActionEnd) {
+    while (interpreter->actions[interpreter->action_read_idx].action != ActionEnd)
         interpreter_execute_action(interpreter, interpreter->actions[interpreter->action_read_idx++]);
-    }
+
     free(interpreter->actions);
 }
 
-static void execute_actions(struct Action *actions) {
-    struct Interpreter interpreter;
+static void interpreter_reset(struct Interpreter *interpreter) {
     size_t i;
 
-    interpreter.actions = actions;
-    interpreter.action_read_idx = 0;
-    interpreter.tape_idx = 0;
+    interpreter->action_read_idx = 0;
+    interpreter->tape_idx = 0;
     for (i = 0; i < TAPE_SIZE; ++i)
-        interpreter.tape[i] = 0;
-    interpreter_execute(&interpreter);
+        interpreter->tape[i] = 0;
 }
 
 static void execute_string(char *string) {
-    enum TokenType *tokens;
-    struct Action *actions;
+    struct Tokenizer tokenizer;
+    struct Interpreter interpreter;
 
-    tokens = tokenize_string(string);
-    actions = parse_tokens(tokens);
-    execute_actions(actions);
+    /* set up tokenizer to read from a string, set the string's value and
+       and reset all other attributes */
+    tokenizer.read_type = ReadFromString;
+    tokenizer.read_from.string = string;
+    tokenizer.allocated = tokenizer.idx = 0;
+    tokenizer_tokenize(&tokenizer);
+
+    /* create interpreter and run it's actions */
+    interpreter_reset(&interpreter);
+    interpreter.actions = parse_tokens(tokenizer.tokens);
+    interpreter_execute(&interpreter);
 }
 
 static void execute_file(const char *filename) {
-    enum TokenType *tokens;
-    struct Action *actions;
+    struct Tokenizer tokenizer;
+    struct Interpreter interpreter;
 
-    tokens = tokenize_file(filename);
-    actions = parse_tokens(tokens);
-    execute_actions(actions);
+    tokenizer.read_type = ReadFromFile;
+    tokenizer.read_from.file = fopen(filename, "r"); /* open file */
+    if (!tokenizer.read_from.file) {
+        perror("fopen");
+        exit(1);
+    }
+    tokenizer.allocated = tokenizer.idx = 0;
+    tokenizer_tokenize(&tokenizer);
+    fclose(tokenizer.read_from.file);
+
+    /* create interpreter and run it's actions */
+    interpreter_reset(&interpreter);
+    interpreter.actions = parse_tokens(tokenizer.tokens);
+    interpreter_execute(&interpreter);
+}
+
+static void execute_repl(void) {
+    struct Tokenizer tokenizer;
+    struct Interpreter interpreter;
+
+    /* set up tokenizer to read from stdin and reset it's attributes */
+    tokenizer.read_type = ReadFromInput;
+    interpreter_reset(&interpreter);
+
+    for (;;) {
+        tokenizer.allocated = tokenizer.idx = 0;
+        interpreter.action_read_idx = 0;
+        printf("bf> ");
+        tokenizer_tokenize(&tokenizer);
+        interpreter.actions = parse_tokens(tokenizer.tokens);
+        interpreter_execute(&interpreter);
+    }
 }
 
 static void print_usage(void) {
-    printf("bf [filename]\n");
-    printf("bf -s [string of code to execute]\n");
+    printf("run file: bf [filename]\n");
+    printf("run string: bf -s [string of code to execute]\n");
+    printf("run repl: bf -i\n");
 }
 
 int main(int argc, char **argv) {
@@ -356,6 +398,10 @@ int main(int argc, char **argv) {
         }
         for (i = 0; i < argc; ++i)
             execute_string(argv[i]);
+    } else if (strcmp(argv[0], "-i") == 0) {
+        ++argv;
+        --argc;
+        execute_repl();
     } else {
         for (i = 0; i < argc; ++i)
             execute_file(argv[i]);
